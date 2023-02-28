c115b0696f8d35f26b287d6e2a9438d0cdedc0de
Chaleira <pedromchaleira@gmail.com>
2023-02-28 19:19:37 +0000

    abc

--- client.c
+++ client.c
@@ -6,56 +6,58 @@
 /*   By: chales <chales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 12:20:41 by chales            #+#    #+#             */
-/*   Updated: 2023/02/27 18:01:08 by chales           ###   ########.fr       */
+/*   Updated: 2023/02/28 16:58:05 by chales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include "libft/libft.h"
 #include <unistd.h>
+#include <signal.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "libft/libft.h"
 
 void	sender(char c, pid_t pid)
 {
 	int	i;
 
-	i = 0;
-	while (i < 8)
+	i = 7;
+	while (i >= 0)
 	{
-		if ((c & i) == 1)
+		if (c & (1 << i))
 		{
 			if (kill(pid, SIGUSR1) == -1)
 			{
 				ft_printf("Error!\n");
-				return ;
+				exit(EXIT_FAILURE);
 			}
 		}
-		else if ((c & i) == 0)
+		else
 		{
 			if (kill(pid, SIGUSR2) == -1)
 			{
 				ft_printf("Error!\n");			
-				return ;
+				exit(EXIT_FAILURE);
 			}
 		}
-		i++;
-		usleep(500);
+		i--;
 	}
 }
 
 int main(int ac, char	*av[])
 {
 	pid_t	pid;
-	int		i;
-
-	i = 0;
+	int		j;
+	char	*str;
+	
+	j = 0;
 	if (ac != 3)
 		return (0);
 	pid = ft_atoi(av[1]);
-	while (av[2][i] && av[2][i] != '\0')
+	str = ft_strdup(av[2]);
+	while (str && str[j] != '\0')
 	{
-		sender(av[2][i], pid);
-		i++;
+		sender(str[j], pid);
+		j++;
 	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,51 +6,39 @@
 /*   By: chales <chales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 13:39:03 by chales            #+#    #+#             */
-/*   Updated: 2023/02/27 18:29:06 by chales           ###   ########.fr       */
+/*   Updated: 2023/02/28 19:15:02 by chales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "libft/libft.h"
 
 void	handler(int signal)
 {
-	static	int		i;
-	static	char	c;
+	static char	c;
+	static int	bit;
 
-	if (i == 8)
+	c |= (signal == SIGUSR1) << bit;
+	if (bit == 7)
 	{
-		write (1, &c, 1);
-		i = 0;
-		c = 0;
+		ft_printf("%c", c);
+		bit = 0;
 	}
-	
-	if (signal == SIGUSR1)
-	{
-		ft_printf("1\n");
-		c |= (1 << i);
-		i++;
-	}
-	else if (signal == SIGUSR2)
-	{
-		ft_printf("0\n");
-		c |= (0 << i);
-		i++;
-	}
+	bit++;
 }
 
 int	main(void)
 {
 	pid_t	pid;
-	
+
 	pid = getpid();
 	ft_printf("PID : %d\n", pid);
 	signal(SIGUSR1, handler);
 	signal(SIGUSR2, handler);
 	while (1)
-	{
 		pause();
-	}
-	return (0);
+	return (EXIT_FAILURE);
 }
